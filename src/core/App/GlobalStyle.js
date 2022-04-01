8116571978fbd62de36e5594987c3fa50c9a446d
Dorota Borowska <youcode2021db@gmail.com>
2022-04-01 21:03:45 +0200

    GlobalStyle created

---
+++ src/core/App/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+    box-sizing: inherit;
+    }
+
+    body {
+    font-family: "Inter", sans-serif;
+    transition: background 0.3s;
+    background-color: ${({ theme }) => theme.color.mercury};
+    font-size: 18px;
+    letter-spacing: 0.05em;
+    word-break: break-word; //by linki przerzucaly sie do nowej linii
+    overflow-y: scroll;
+    padding-bottom:  108px; //by odstep na dole byl
+    margin-top: 46px;
+
+  
+
+
+    }
+;`
