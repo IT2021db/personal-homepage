c51ad037e9582e73ad790b1e7b071542ab1c94e6
Dorota Borowska <youcode2021db@gmail.com>
2022-04-01 21:09:55 +0200

    theme created

---
+++ src/core/App/theme.js
@@ -0,0 +1,19 @@
+export const themeLight = {
+    color: {
+        black: "#000",
+        mineShaft: "#252525",
+        silver: "#ccc",
+        slateGray: "#6E7E91",
+        alto: "#ddd",
+        gallery: "#eee",
+        white: "#fff",
+        whiteLilac: "#FBFBFE",
+        mercury: "#E5E5E5",
+        teal: "#008080",
+        cardinal: "#c41e3a",
+        forestGreen: "#228c22",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
