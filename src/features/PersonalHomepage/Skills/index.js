1711bf81e3faf0e49d7bd872d713a4d6bdcddef3
Dorota Borowska <youcode2021db@gmail.com>
2022-04-03 13:24:47 +0200

    icon added

--- src/features/PersonalHomepage/Skills/index.js
+++ src/features/PersonalHomepage/Skills/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import { Item, List, Section, StyleHeader } from "./styled";
 
 export const Skills = () => (
-    <Section>
-        <StyleHeader>My skillset </StyleHeader>
-        <List>
-            <Item>praca domowa</Item>
-        </List>
-    </Section>
+    <>
+        <Section>
+            <StyleHeader>My skillset includes 🛠️ </StyleHeader>
+            <List>
+                <Item>praca domowa</Item>
+            </List>
+        </Section>
+        <Section>
+            <StyleHeader>What I want to learn next 🚀 </StyleHeader>
+            <List>
+                <Item>praca domowa</Item>
+            </List>
+        </Section>
+    </>
 );
