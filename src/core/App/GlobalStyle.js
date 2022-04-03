7fd897381fc0294b329ba6846805585f23328e9e
Dorota Borowska <youcode2021db@gmail.com>
2022-04-03 13:22:06 +0200

    background coleor changed

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -12,7 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
     font-family: "Inter", sans-serif;
     transition: background 0.3s;
-    background-color: ${({ theme }) => theme.color.mercury};
+    background-color: ${({ theme }) => theme.color.whiteLilac};
     font-size: 18px;
     letter-spacing: 0.05em;
     word-break: break-word; //by linki przerzucaly sie do nowej linii
