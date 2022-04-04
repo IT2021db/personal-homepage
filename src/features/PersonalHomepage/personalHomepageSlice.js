8c1368b275bbfbdc7b3d4e9bfd889eafbdc0e81b
Dorota Borowska <youcode2021db@gmail.com>
2022-04-04 23:04:49 +0200

    reucers for repositories feching  created

---
+++ src/features/PersonalHomepage/personalHomepageSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@redux/toolkit";
+
+const personalHomePageSlice = createSlice({
+    name: "personalHomepage",
+    initialState: {
+        repositories: null,
+        status: "initial",
+
+    },
+    reducers: {
+        fetchRepositories: () => ({
+            status: "loading",
+            repositories: null,
+        }),
+        fetchRepositoriesSuccess: (_, { payload: repositories }) => ({
+            status: "success",
+            repositories,
+        }),
+        fetchrepositoriesError: () => ({
+            status: "error",
+            repositories: null,
+        }),
+    },
+});
+
+export const {
+    fetchRepositories,
+    fetchRepositoriesSuccess,
+    fetchrepositoriesError,
+} = personalHomePageSlice.actions;
+
+const selectPersonalHomePageState = state => state.personalHomePageState;
+
+export const selectrepositories = state => selectPersonalHomePageState(state).repositories;
+export const selectRepositoriesStatus = state => selectPersonalHomePageState(state).status;
+
+export default personalHomePageSlice.reducer;
