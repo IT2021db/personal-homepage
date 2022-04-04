71dbe6869d72a81d3071c47b08dab8696a0705a0
Dorota Borowska <youcode2021db@gmail.com>
2022-04-04 03:39:27 +0200

    Normalize from styled-normalize implemented

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,10 +2,12 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { themeLight } from "./theme";
 import { PersonalHomepage } from "../../features/PersonalHomepage";
+import { Normalize } from "styled-normalize";
 
- function App() {
+function App() {
   return (
     <ThemeProvider theme={themeLight}>
+      <Normalize />
       <GlobalStyle />
       <PersonalHomepage />
     </ThemeProvider>
