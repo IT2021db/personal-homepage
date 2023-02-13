37a119776be44818e0d204ea521356e2623840eb
Dorota Borowska <youcode2021db@gmail.com>
2023-02-13 15:49:24 +0100

    useState, useEffect

--- src/features/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -1,30 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Section } from "../Skills/styled";
 import { MyRecentProjects, SubHeader, Header, StyledGithubIcon } from "./styled";
 import { Content } from "./Content";
 import { GetApi } from "./GetApi";
 import { githubUsername } from "./githubUsername";
-
-export const Portfolio = () => {
-    // const dispatch = useDispatch;
-
-    // const repositoriesStatus = useSelector(selectRepositoriesStatus);
-    // const repositories = useSelector(selectRepositories);
-import React from "react";
-//import { Section } from "../Skills/styled";
-import { Title, SubHeader, StyledGithubIcon, Section } from "./styled";
-//import { Content } from "../Content";
+import { Title,  } from "./styled";
 import { Tiles } from "./Tiles";
 
-export const Portfolio = () => (
-    <Section>
-        <StyledGithubIcon />
-        <SubHeader>Portfolio </SubHeader>
-        <Title>My recent projects</Title>
-        <Tiles />
-    </Section>
 
+
+export const Portfolio = () => {
+  
+const [state, setState]=useState()
+// useEffect (()=>{
+//     fetch("https://api.github.com/users/IT2021db/repos").then(res=>setState(res.data))
+// })
 
     // useEffect(()=>{
     //     dispatch(fetchRepositories(githubUsername));
@@ -35,14 +26,19 @@ export const Portfolio = () => (
             <Header>
                 <StyledGithubIcon />
                 <SubHeader>Portfolio </SubHeader>
-                <MyRecentProjects>My recent projects</MyRecentProjects>
+                <MyRecentProjects>My recent projects </MyRecentProjects>
             </Header>
             <Content
             // status={repositoriesStatus}
             // repositories={repositories}
-
+            
+          
+            
             />
+       <>
+       </>
+              <button>figa</button>
             <GetApi />
         </Section>
     );
-}
+};
