0e2059d015bfa6fb02f120292af6545d9ac11241
Dorota Borowska <youcode2021db@gmail.com>
2023-02-19 19:17:04 +0100

    delete

--- src/features/PersonalHomepage/personalHomepageAPI.js
+++
@@ -1,7 +0,0 @@
-// import axios from "axios";
-
-// const githubAPIBaseURL = "https://api.github.com";
-
-// export const getRepositories = username =>
-//     axios.get(`${githubAPIBaseURL}/users/${username}/repos`)
-//         .then(response => response.data);
