b559a6e09ad3aa92d049f008c4266f339cee86fe
Dorota Borowska <youcode2021db@gmail.com>
2023-02-21 17:40:36 +0100

    Provider added

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,19 +1,22 @@
 import { ThemeProvider } from "styled-components";
-//import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./GlobalStyle";
 import { themeLight } from "./theme";
 import { PersonalHomepage } from "../../features/PersonalHomepage";
 import { Normalize } from "styled-normalize";
+import store from "./store"
 
 function App() {
   return (
- 
+
+    <Provider store={store}>
       <ThemeProvider theme={themeLight}>
-      <Normalize />
-      <GlobalStyle />
-      <PersonalHomepage />
-    </ThemeProvider>
-  
+        <Normalize />
+        <GlobalStyle />
+        <PersonalHomepage />
+      </ThemeProvider>
+    </Provider>
+
   );
 }
 
