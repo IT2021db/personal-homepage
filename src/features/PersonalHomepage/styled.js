2ee130035f535c297baf4af0c622de1ebd22f4ab
Dorota Borowska <youcode2021db@gmail.com>
2023-02-22 20:25:58 +0100

    styled.p

--- src/features/PersonalHomepage/styled.js
+++ src/features/PersonalHomepage/styled.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { theme } from "../../core/App/theme";
 
-export const Container = styled.div`
+export const Container = styled.p`
     max-width: 1270px;
     height:2339px ; //100vh
     margin: auto;
-    padding:115px;//?
+    
+  
    
 `;
