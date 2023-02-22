ab93cb78b734ce1209153cee8e345baa8cf81d42
Dorota Borowska <youcode2021db@gmail.com>
2023-02-22 11:02:27 +0100

    store connected to saga

--- src/core/App/store.js
+++ src/core/App/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homepageReducer from "../../features/PersonalHomepage/homepageSlice";
+import createSagaMiddleware from "redux-saga";
+import { watchHomepageSaga } from "../../features/PersonalHomepage/homepageSaga";
+import rootSaga from "../../rootSaga";
+import { homepageSaga } from "../../features/PersonalHomepage/homepageSaga"
+//import { getRepos } from "../../features/PersonalHomepage/getRepos";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-    reducer:{
+    reducer: {
         homepage: homepageReducer
-    }
-
+    },
+    middleware: [sagaMiddleware],
 });
+
+//getRepos().then(ZMIENNA => console.log(ZMIENNA));
+
+sagaMiddleware.run(watchHomepageSaga)
 export default store;
