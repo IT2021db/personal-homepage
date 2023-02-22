a6b46c54fd85fe6adde61dee5301fd459489ae02
Dorota Borowska <youcode2021db@gmail.com>
2023-02-22 15:25:02 +0100

    throw new error added

--- src/features/PersonalHomepage/getRepos.js
+++ src/features/PersonalHomepage/getRepos.js
@@ -1,5 +1,9 @@
 export const getRepos = async () => {
     const response = await fetch("http://api.github.com/users/IT2021db/repos");
-    const repos = await response.json();
-    return repos;
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return await response.json();
 };
