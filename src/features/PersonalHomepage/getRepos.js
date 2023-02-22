db218561a4b9b64b90ad47a80adc9e5e7f01aecd
Dorota Borowska <youcode2021db@gmail.com>
2023-02-22 13:13:20 +0100

    code rebuild

--- src/features/PersonalHomepage/getRepos.js
+++ src/features/PersonalHomepage/getRepos.js
@@ -1,8 +1,5 @@
 export const getRepos = async () => {
-    const response = await fetch("http://api.github.com/users/ID2021db/repos");
-
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-    return await response.json();
+    const response = await fetch("http://api.github.com/users/IT2021db/repos");
+    const repos = await response.json();
+    return repos;
 };
