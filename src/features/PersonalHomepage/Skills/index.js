d6b3a5e2c6f2bf6c766977a68cbfe3b35c155ca0
Dorota Borowska <youcode2021db@gmail.com>
2023-03-03 13:13:36 +0100

    ListSection component placed instead all

--- src/features/PersonalHomepage/Skills/index.js
+++ src/features/PersonalHomepage/Skills/index.js
@@ -1,36 +1,11 @@
 import React from "react";
-import { Item, List, Section, StyleHeader } from "./styled";
+import { skillset } from "./skillset";
+import { toLearn } from "./toLearn";
+import { ListSection } from "./ListSection/index.js";
 
-export const Skills = () => (
+export const Skills = (itemSet, title) => (
     <>
-        <Section>
-            <StyleHeader>My skillset includes 🛠️ </StyleHeader>
-            <List>
-                <Item>Semantic & accessible HTML</Item>
-                <Item>Responsive Web Design</Item>
-                <Item>Teamwork</Item>
-                <Item>Markdown</Item>
-                <Item>Immutability</Item>
-                <Item>CSS BEM convention</Item>
-                <Item>CSS Grid</Item>
-                <Item>CSS Flexbox</Item>
-            </List>
-
-            <List>
-                <Item>React Router</Item>
-                <Item>Redux-Saga</Item>
-                <Item>Redux (toolkit)</Item>
-                <Item>React Hooks</Item>
-                <Item>Error Handling</Item>
-                <Item>Working with Api (fetch, axios)</Item>
-                <Item>JavaScript: ES6+</Item>
-            </List>
-        </Section>
-        <Section>
-            <StyleHeader>What I want to learn next 🚀 </StyleHeader>
-            <List>
-                <Item>praca domowa</Item>
-            </List>
-        </Section>
+        <ListSection title="My skills includes 🛠️ " itemSet={skillset} />
+        <ListSection title="WhatI want to learn next 🚀" itemSet={toLearn} />
     </>
 );
