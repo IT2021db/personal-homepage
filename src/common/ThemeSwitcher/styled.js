6e520c73b2b85974259830ecacf198f0312d4a81
Dorota Borowska <youcode2021db@gmail.com>
2023-03-06 14:11:13 +0100

    creation 'lightTheme'

---
+++ src/common/ThemeSwitcher/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { ReactComponent as SunIcon } from "./brightness 1.svg";
+import { ReactComponent as SwitchIcon } from "./Vector (3).svg";
+
+export const SwitchContainer=styled.div`
+display: flex;
+justify-content: flex-end;
+
+`;
+
+export const SwitchText = styled.p``;
+
+export const Icon = styled(SwitchIcon)`
+    background-color:grey;
+`;
+export const Sun = styled(SunIcon)`
+   
+`;
--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,4 +1,22 @@
-export const themeLight = {
+export const lightTheme = {
+    color: {
+        black: "#000",          //black
+        mineShaft: "#252525",   //mineShaft
+        silver: "#ccc",         //silverGray
+        slateGray: "#6E7E91",   //slateGray
+        gallery: "#EFEFEF",     //gallery
+        iron: "#D1D5DA",        //iron
+        white: "#fff",          //white
+        whiteLilac: "#FBFBFE",  //whiteLilac
+        mercury: "#E5E5E5",     //mercury
+        scienceBlue: "#0366D6"  //scienceBlue
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
+
+export const darkTheme = {
     color: {
         black: "#000",
         mineShaft: "#252525",
@@ -6,7 +24,7 @@ export const themeLight = {
         slateGray: "#6E7E91",
         alto: "#ddd",
         gallery: "#EFEFEF",
-        iron:"#D1D5DA",
+        iron: "#D1D5DA",
         white: "#fff",
         whiteLilac: "#FBFBFE",
         mercury: "#E5E5E5",
@@ -14,9 +32,27 @@ export const themeLight = {
         cardinal: "#c41e3a",
         forestGreen: "#228c22",
         scienceBlue: "#0366D6",
-        whiteLilac:"#FBFBFE"
+        whiteLilac: "#FBFBFE"
     },
     breakpoint: {
         mobileMax: 767,
     },
 };
+
+const colorName = {
+    black: "#000",
+    mineShaft: "#252525",
+    silver: "#ccc",
+    slateGray: "#6E7E91",
+    alto: "#ddd",
+    gallery: "#EFEFEF",
+    iron: "#D1D5DA",
+    white: "#fff",
+    whiteLilac: "#FBFBFE",
+    mercury: "#E5E5E5",
+    teal: "#008080",
+    cardinal: "#c41e3a",
+    forestGreen: "#228c22",
+    scienceBlue: "#0366D6",
+    whiteLilac: "#FBFBFE"
+}
