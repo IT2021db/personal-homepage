e6fedf3e9803d106b65b35cc47ab43e37910d37e
Dorota Borowska <youcode2021db@gmail.com>
2023-03-07 23:36:36 +0100

    Provider, normalize moved outside app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './core/App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from "react-redux";
+import { Normalize } from "styled-normalize";
+import store from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Normalize />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
