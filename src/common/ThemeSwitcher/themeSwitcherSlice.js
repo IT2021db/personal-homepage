21b02390814c437fec5a3c90d2c12e99dbeb9748
Dorota Borowska <youcode2021db@gmail.com>
2023-03-07 23:43:24 +0100

    themeSwitcherSlice created

---
+++ src/common/ThemeSwitcher/themeSwitcherSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSwitcherSlice = createSlice({
+    name: "theme",
+    initialState: {
+        isDark: false,
+    },
+    reducers: {
+        toggleTheme: state => {
+            state.isDark = !state.isDark;
+            console.log(`stan w reducerze${state.isDark}`)
+        },
+    }
+});
+
+export const { toggleTheme } = themeSwitcherSlice.actions;
+
+const selectThemeState = state => state.theme;
+export const selectIsDark = state => state.theme.isDark;
+
+
+//export const selectIsDark = state => selectThemeState(state).isDark;
+//export const selectIsDark = state => selectThemeState(state).isDark;
+
+export default themeSwitcherSlice.reducer;
+
+console.log(selectIsDark)
+console.log(selectThemeState)
