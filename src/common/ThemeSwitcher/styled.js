0dcd43dfc5d36e981be372335dff7f2e05de51f1
Dorota Borowska <youcode2021db@gmail.com>
2023-03-07 23:42:56 +0100

    uppercase added in SwitchText

--- src/common/ThemeSwitcher/styled.js
+++ src/common/ThemeSwitcher/styled.js
@@ -1,13 +1,14 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as SunIcon } from "./brightness 1.svg";
 
-export const SwitchContainer=styled.div`
+export const SwitchContainer = styled.div`
 display: flex;
 justify-content: flex-end;
-
 `;
 
-export const SwitchText = styled.p``;
+export const SwitchText = styled.p`
+    text-transform: uppercase;
+`;
 
 export const Icon = styled.span`
     background-color:grey;
@@ -15,7 +16,6 @@ export const Icon = styled.span`
     margin-top:15px;
     height: 25px;
     width: 48px;
-`;
+  `;
 export const Sun = styled(SunIcon)`
-   
 `;
