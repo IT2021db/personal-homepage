12d7826dd6744e843cb00f7f473dccb3d6875ac2
Dorota Borowska <youcode2021db@gmail.com>
2023-03-07 23:42:03 +0100

    onClick  dispatches the switcher to change theme

--- src/common/ThemeSwitcher/index.js
+++ src/common/ThemeSwitcher/index.js
@@ -1,14 +1,17 @@
-import {SwitchContainer, SwitchText, Icon, Sun } from "./styled";
+import { useDispatch, useSelector } from "react-redux";
+import { SwitchContainer, SwitchText, Icon, Sun } from "./styled";
+import { selectIsDark, toggleTheme } from "./themeSwitcherSlice";
 
 export const ThemeSwitcher = () => {
+    const dispatch = useDispatch();
+    const isDark = useSelector(selectIsDark);
+    console.log(`isDark in ThemeSwitcher: ${isDark}`)
     return (
-     
-         <SwitchContainer>
-               <SwitchText>DARK MODE OFF&nbsp;&nbsp; </SwitchText>
-            <Icon>
+        <SwitchContainer onClick={() => dispatch(toggleTheme())}>
+            <SwitchText>DARK MODE {isDark ? "on" : "off"} </SwitchText>
+            <Icon isDark={isDark}>
                 <Sun />
             </Icon>
-         </SwitchContainer>
-       
+        </SwitchContainer>
     )
 };
