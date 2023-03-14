48e9112e78d3d57b6e0ef8e29d7d9fe4d9c0f303
Dorota Borowska <youcode2021db@gmail.com>
2023-03-14 17:29:42 +0100

    padding, dimention changed

--- src/features/PersonalHomepage/styled.js
+++ src/features/PersonalHomepage/styled.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.p`
-    max-width: 1270px;
-    height:2339px ; //100vh
+export const Container = styled.div`
+    max-width: 1248px;
+    padding:16px;
     margin: auto;
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobileMax}){
+        padding:8px;
+    }
     
   
    
