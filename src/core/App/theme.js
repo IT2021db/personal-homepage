bfbf8aacf7f91e73cfa545e504f6b8d695b0cae3
Dorota Borowska <youcode2021db@gmail.com>
2023-03-14 15:58:57 +0100

    breakpoints for mobiles added

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,37 +1,39 @@
+const breakpoints = {
+    mobileMax: "768px",
+    tebletVerticalMax: "991px",
+    tabletHorizonMax: "1199px"
+}
+
 export const lightTheme = {
+    breakpoints: breakpoints,
     color: {
-        black: "#000",                  
-        headerTextColor: "#252525",    
-        secondaryTextColor: "#6E7E91",                 
-        talesBorder: "#D1D5DA",         
-        white: "#fff",                   
-        sectionBackgroundColor: "#fff",        
-        generalBackground: "#FBFBFE",    
-        switcherBackground: "#E5E5E5",  
+        black: "#000",
+        headerTextColor: "#252525",
+        secondaryTextColor: "#6E7E91",
+        talesBorder: "#D1D5DA",
+        white: "#fff",
+        sectionBackgroundColor: "#fff",
+        generalBackground: "#FBFBFE",
+        switcherBackground: "#E5E5E5",
         switcherBorder: "#6E7E91",
-        blueElementColor: "#0366D6" ,    
-        sunColor:"#FFFF"
-    },
-    breakpoint: {
-        mobileMax: 767,
+        blueElementColor: "#0366D6",
+        sunColor: "#FFFF"
     },
 };
 
 export const darkTheme = {
+    breakpoints: breakpoints,
     color: {
-        black: "#000",                     
-        headerTextColor: "#fff",           
-        secondaryTextColor: "#fff",                              
+        black: "#000",
+        headerTextColor: "#fff",
+        secondaryTextColor: "#fff",
         talesBorder: "rgba(209, 213, 218, 0.1)",
-        white: "#fff",                    
+        white: "#fff",
         sectionBackgroundColor: "rgba(54, 54, 54, 0.72)",
-        generalBackground: "#252525",      
-        blueElementColor: "#0366D6" ,        
-        switcherBackground:"rgba(54, 54, 54, 0.72)",
+        generalBackground: "#252525",
+        blueElementColor: "#0366D6",
+        switcherBackground: "rgba(54, 54, 54, 0.72)",
         switcherBorder: "#FFFF",
-        sunColor:"#000"
-    },
-    breakpoint: {
-        mobileMax: 767,
+        sunColor: "#000"
     },
 };
