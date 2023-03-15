e810e0039a302c7d2bde415152eaf274919b62e2
Dorota Borowska <youcode2021db@gmail.com>
2023-03-15 17:58:00 +0100

    ButtonLink imported

--- src/features/PersonalHomepage/Portfolio/Content/ErrorBox/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/ErrorBox/index.js
@@ -1,5 +1,7 @@
 import { ReactComponent as WarningIcon } from "./warning.svg";
 import { Wrapper, Header, Paragraph } from "./styled.js";
+import { ButtonLink } from "../../../ButtonLink/styled";
+import { githubUsername } from "../../../githubUsername";
 
 export const ErrorBox = () => (
     <Wrapper>
@@ -9,6 +11,13 @@ export const ErrorBox = () => (
             Sorry, failed to load Github projects.<br />
             You can check them directly on Github.
         </Paragraph>
+        <ButtonLink
+            href={`https://github.com/${githubUsername}`}
+            target="_blank"
+            rel="noreferrer noopener"
+        >
+            Go to Github
+        </ButtonLink>
     </Wrapper>
 
 );
