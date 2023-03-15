9c325eb3b9536042eaf8584bb16d75fad4580e38
Dorota Borowska <youcode2021db@gmail.com>
2023-03-15 15:10:10 +0100

    ErrorBox added

--- src/features/PersonalHomepage/Portfolio/Content/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/index.js
@@ -3,12 +3,19 @@ import { Loading } from "./Loading";
 import { Repositories } from "./Repositories";
 
 export const Content = ({ status, repos }) => {
+    console.log(`status ${status}`)
+    console.log(`repos w Content:`)
+    
+    console.log({repos})
     switch (status) {
         case "initial":
             return null;
 
         case "loading":
             return <Loading />
+
+        case "error":
+             return <ErrorBox/>;
 
         case "success":
             return <Repositories repos={repos} />
