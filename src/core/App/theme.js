e0010e3406f153663fc79dc7cce27c1695cae255
Dorota Borowska <youcode2021db@gmail.com>
2023-03-16 15:28:31 +0100

    talesBorderHover added

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -10,14 +10,15 @@ export const lightTheme = {
         black: "#000",
         headerTextColor: "#252525",
         secondaryTextColor: "#6E7E91",
-        talesBorder: "#D1D5DA",
         white: "#fff",
         sectionBackgroundColor: "#fff",
         generalBackground: "#FBFBFE",
         switcherBackground: "#E5E5E5",
         switcherBorder: "#6E7E91",
         blueElementColor: "#0366D6",
-        sunColor: "#FFFF"
+        sunColor: "#FFFF",
+        talesBorder: "rgba(209, 213,218, 0.3)",
+        talesBorderHover: "rgba(9,10,51, 0.3)"
     },
 };
 
@@ -26,14 +27,15 @@ export const darkTheme = {
     color: {
         black: "#000",
         headerTextColor: "#fff",
-        secondaryTextColor: "#fff",
-        talesBorder: "rgba(209, 213, 218, 0.1)",
+        secondaryTextColor: "#fff", 
         white: "#fff",
         sectionBackgroundColor: "rgba(54, 54, 54, 0.72)",
         generalBackground: "#252525",
         blueElementColor: "#0366D6",
         switcherBackground: "rgba(54, 54, 54, 0.72)",
         switcherBorder: "#FFFF",
-        sunColor: "#000"
+        sunColor: "#000",
+        talesBorder: "rgba(209, 213, 218, 0.1)",
+        talesBorderHover: "rgba(9, 10, 51, 0.03)",
     },
 };
