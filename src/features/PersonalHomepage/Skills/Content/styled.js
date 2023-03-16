92bdc8aabd72792948a1ba093920da620c1a0514
Dorota Borowska <youcode2021db@gmail.com>
2023-03-16 17:16:15 +0100

    paddings, margin, @media added

--- src/features/PersonalHomepage/Skills/Content/styled.js
+++ src/features/PersonalHomepage/Skills/Content/styled.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-import {ReactComponent as Icon}from"./dot.svg";
+import { ReactComponent as Icon } from "./dot.svg";
 
 export const Section = styled.section`
     margin-top: 63px;
-    padding:32px 0;
+    padding:12px 0;
+    padding-left:20px;
     background-color: ${({ theme }) => theme.color.sectionBackgroundColor};
 
-    @media(max-width:${({theme})=>theme.breakpoints.mobileMax}){
-        margin:48px 0;
-        padding:0;
+    @media(max-width:${({ theme }) => theme.breakpoints.mobileMax}){
+        margin:38px 0;
+        padding-left:10px;
     }
 `;
 
@@ -20,22 +21,43 @@ export const Title = styled.h3`
     border-bottom: 1px solid ${({ theme }) => theme.color.talesBorder};
     color: ${({ theme }) => theme.color.headerTextColor};
 
-    @media(max-width:${({theme})=>theme.breakpoints.mobileMax}){
-       font-size:18px;
+    @media(max-width:${({ theme }) => theme.breakpoints.mobileMax}){
+       font-size:12px;
     }
 
 `;
 
 export const List = styled.ul`
- color: ${({ theme }) => theme.color.secondaryTextColor};
- display: grid;
- grid-template-columns:repeat(2, 1fr);
- grid-gap:5px;
- list-style:none;
+    padding:0;
+    color: ${({ theme }) => theme.color.secondaryTextColor};
+    display: grid;
+    grid-template-columns:repeat(3, 1fr);
+    grid-gap:8px 32px;
+    list-style:none;
+
+    @media(max-width:${({ theme }) => theme.breakpoints.mobileMax}){
+        grid-template-columns:1fr;
+        font-size: 12px;
+    }
 `;
 
-export const Item = styled.li``;
+export const Item = styled.li`
+    display:flex;
+    line-height: 1.4;
+    letter-spacing:0;
 
-export const Bullet=styled(Icon)`
-color: ${({ theme }) => theme.color.blueElementColor};
+    @media(max-width:${({ theme }) => theme.breakpoints.mobileMax}){
+       line-height: 1.2;
+    }
+`;
+
+export const Bullet = styled(Icon)`
+    color: ${({ theme }) => theme.color.blueElementColor};
+    margin-right:16px;
+    height: auto;
+
+    @media(max-width:${({ theme }) => theme.breakpoints.mobileMax}){
+       width: 6px;
+       margin-right: 8px;
+    }
 `;
