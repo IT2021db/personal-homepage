44faf775d7cd78264b2e5da43927a5a169b656ef
Dorota Borowska <youcode2021db@gmail.com>
2023-03-16 14:37:27 +0100

    list-style:none in List, Tile change to li, hover

--- src/features/PersonalHomepage/Portfolio/Content/Repositories/styled.js
+++ src/features/PersonalHomepage/Portfolio/Content/Repositories/styled.js
@@ -1,17 +1,35 @@
 import styled from "styled-components";
 
-export const List=styled.ul`
-display:grid;
-grid-template-columns: repeat(2,1fr);
-grid-gap:32px;
+export const List = styled.ul`
+  display:grid;
+  grid-template-columns: repeat(2,1fr);
+  grid-gap:32px;
+  padding:0;
+  list-style:none;
+
+       @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+          grid-template-columns:1fr;
+          grid-gap:16px;
+        
+  }
 `;
 
-export const Tile = styled.section`
+export const Tile = styled.li`
   border: 6px solid ${({ theme }) => theme.color.talesBorder};
+  transition:border-color 0.3s;
   text-align: left;
-  padding:20px;
+  padding:56px;
+  margin: 0;
   font-size: 18px;
   background-color: ${({ theme }) => theme.color.sectionBackgroundColor};
+
+  &:hover {
+    border-color: ${({theme})=>theme.color.talesBorderHover};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        padding:20px;
+     }
 `;
 
 export const Title = styled.h3`
@@ -23,12 +41,17 @@ export const Title = styled.h3`
 
 export const Description = styled.p`
   margin: 24px 0;
-  line-height: 140%;
+  line-height: 1.4;
   font-size: 18px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        font-size:14px;
+        margin: 16px, 0;  
+     }
 `;
 
 export const LinkWrapper = styled.div`
-padding:8px 0;
+  margin:8px 0;
 `;
 
 export const Link = styled.a` 
