92fff6f766d564560ec30fd5bf415fcddd1b1280
Dorota Borowska <youcode2021db@gmail.com>
2023-03-16 12:27:44 +0100

    margin top set

--- src/features/PersonalHomepage/styled.js
+++ src/features/PersonalHomepage/styled.js
@@ -4,9 +4,12 @@ export const Container = styled.div`
     max-width: 1248px;
     padding:16px;
     margin: auto;
+    margin-top: 20px;
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobileMax}){
-        padding:8px;
+        padding:14px;
+        margin-top:10px;
+       
     }
     
   
