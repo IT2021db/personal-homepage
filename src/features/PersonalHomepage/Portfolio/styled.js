962c51ec7bdd97412eeab5ce3f1c47bb156e51eb
Dorota Borowska <youcode2021db@gmail.com>
2023-03-16 12:41:16 +0100

    clean up

--- src/features/PersonalHomepage/Portfolio/styled.js
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -1,32 +1,46 @@
 import styled from "styled-components";
 import { ReactComponent as GithubIcon } from "./github.svg";
 
+export const Section = styled.section`
+   margin-top:72px;
+
+   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        margin-top:48px;
+     }
+`;
+
+export const StyledGithubIcon = styled(GithubIcon)`
+    margin-bottom:8px ;  
+`;
+
+export const Header = styled.header`
+    text-align: center;
+`;
+
+export const MyRecentRepos = styled.h3`
+    text-align: center;
+    font-weight:normal;
+    margin-top:8px;
+    font-size: 20px;
+    color: ${({ theme }) => theme.color.headerTextColor};
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        font-size:17px;
+     }
+`;
+
+
+
+
 export const SubHeader = styled.header`
     font-size: 30px;
     font-weight: 900;
     letter-spacing: 0.05em;
-    padding-bottom: 16px;
     color: ${({ theme }) => theme.color.headerTextColor};
-`;
 
-export const Title = styled.h3`
-    text-align: center;
-    margin-bottom: 32px;
-    color: ${({ theme }) => theme.color.headerTextColor};
-`;
-export const Header = styled.h3`
-    text-align: center;
-    margin-bottom: 32px;
-`;
-export const Section = styled.section`
-    padding:32px 0 32px 0;
-    background-color: ${({ theme }) => theme.color.generalBackground};
-    text-align: center;
-   
-`;
-
-export const StyledGithubIcon = styled(GithubIcon)`
-    margin-bottom:12px ;
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        font-size:22px;
+     }
 `;
 
 export const ReposSection = styled.ul`
