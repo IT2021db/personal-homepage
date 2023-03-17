449e0cebf7cdb586dd568fe3f0a435cc0fd2315f
Dorota Borowska <youcode2021db@gmail.com>
2023-03-17 11:19:13 +0100

    typo removed in Content title

--- src/features/PersonalHomepage/Skills/index.js
+++ src/features/PersonalHomepage/Skills/index.js
@@ -6,6 +6,6 @@ import { Content } from "./Content/index.js";
 export const Skills = (skills, title) => (
     <>
         <Content title="My skills includes 🛠️ " skills={skillset} />
-        <Content title="WhatI want to learn next 🚀" skills={toLearn} />
+        <Content title="What I want to learn next 🚀" skills={toLearn} />
     </>
 );
