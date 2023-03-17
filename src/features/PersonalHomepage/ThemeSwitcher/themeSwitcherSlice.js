ee6ef5dd9e557a3e3a5e3a122a2323f46859f5b9
Dorota Borowska <youcode2021db@gmail.com>
2023-03-17 11:14:17 +0100

    get from localStorage in initialState

--- src/features/PersonalHomepage/ThemeSwitcher/themeSwitcherSlice.js
+++ src/features/PersonalHomepage/ThemeSwitcher/themeSwitcherSlice.js
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {getIsDarkFromLocalStorage}from "./themeLocalStorage";
 
 const themeSwitcherSlice = createSlice({
     name: "theme",
     initialState: {
-        isDark: false,
+        isDark: getIsDarkFromLocalStorage(),
     },
     reducers: {
         toggleTheme: state => {
@@ -15,11 +16,7 @@ const themeSwitcherSlice = createSlice({
 export const { toggleTheme } = themeSwitcherSlice.actions;
 
 const selectThemeState = state => state.theme;
-export const selectIsDark = state => state.theme.isDark;
-
-
-//export const selectIsDark = state => selectThemeState(state).isDark;
-//export const selectIsDark = state => selectThemeState(state).isDark;
+export const selectIsDark = state =>selectThemeState(state).isDark;
 
 export default themeSwitcherSlice.reducer;
 
