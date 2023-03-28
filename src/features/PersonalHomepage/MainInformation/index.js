40ad8c3c1e29b80faa566f7b9e298f41bf4527d1
Dorota Borowska <youcode2021db@gmail.com>
2023-03-28 21:44:16 +0200

    Paragraph text completed

--- src/features/PersonalHomepage/MainInformation/index.js
+++ src/features/PersonalHomepage/MainInformation/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Paragraph, Wrapper, Name, ThisIs,StyledButton, Icon } from "./styled";
+import { Avatar, Paragraph, Wrapper, Name, ThisIs, StyledButton, Icon } from "./styled";
 import { email } from "../../../common/myEmail";
 import envelope from "./envelope.svg";
 import imgProfile from "./imgProfile.jpg";
@@ -11,7 +11,12 @@ export const MainInformation = () => (
         <div>
             <ThisIs>This is</ThisIs>
             <Name>Dorota Borowska</Name>
-            <Paragraph>Hi! I am a begining Frontend Developer mostly using React</Paragraph>
+            <Paragraph>Hi there! I am a beginning Frontend Developer with a strong focus on React.
+                I&nbsp;thrive on new challenges in the workplace and enjoy discovering new
+                tools and solutions to improve my&nbsp;work. As a team player, I value building strong
+                relationships with my&nbsp;colleagues and believe in the&nbsp;importance of mutual support,
+                sensitivity, and empathy.
+                Outside of work, I&nbsp;enjoy staying active through sports, as well as taking time to relax and enjoy nature.</Paragraph>
             <StyledButton
                 href={`mailto:${email}`}
                 title={email}
